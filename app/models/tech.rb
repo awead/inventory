e16ca62616f905c017c62f593bcd6833e3fed058
Adam Wead <amsterdamos@gmail.com>
2012-08-06 19:55:40 -0400

    Adding field to app

--- app/models/tech.rb
+++ app/models/tech.rb
@@ -4,15 +4,18 @@ class Tech < ActiveRecord::Base
   validates_presence_of :name, :vendor, :tech_type
   validates_uniqueness_of :serial
   validates_uniqueness_of :tech_mac, :allow_nil => true, :allow_blank => true
+  validates_uniqueness_of :wifi_mac, :allow_nil => true, :allow_blank => true
   validates_uniqueness_of :tech_ip, :allow_nil => true, :allow_blank => true
   validates_format_of :tech_mac, :with => /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/,
+    :message => "Please use the format XX:XX:XX:XX:XX:XX for the MAC address"
+  validates_format_of :wifi_mac, :with => /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/,
     :message => "Please use the format XX:XX:XX:XX:XX:XX for the MAC address"
   validates_format_of :tech_ip, :with => /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?$/,
     :message => "Please use the format ###.###.###.### for the IP"
   belongs_to :room
 
-  @sql_list = 
-    "select a.name, a.model, a.tech_type, a.vendor, a.serial, a.tech_mac, a.description, a.room_id, a.id, a.tech_ip, a.net_name, " +
+  @sql_list =
+    "select a.name, a.model, a.tech_type, a.vendor, a.serial, a.tech_mac, a.wifi_mac, a.description, a.room_id, a.id, a.tech_ip, a.net_name, " +
     "b.room_name, b.room_number " +
     "from teches a, rooms b where a.room_id = b.id"
 
